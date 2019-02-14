e425c8c407564c804d68de0bf2134d468d166696
Kilian <kilian.paquier@hotmail.fr>
2019-02-14 16:56:57 +0100

    erreurs

--- model/centre.cpp
+++ model/centre.cpp
@@ -4,3 +4,48 @@ Centre::Centre()
 {
 
 }
+
+Centre::~Centre()
+{
+
+}
+
+vector<Patient> &Centre::getPatients()
+{
+
+}
+
+vector<Personnel> &Centre::getPersonnels()
+{
+
+}
+
+void Centre::setPatients(vector<Patient> &patients)
+{
+
+}
+
+void Centre::setPersonnels(vector<Personnel> &personnels)
+{
+
+}
+
+void Centre::addPatient(Patient &patient)
+{
+
+}
+
+void Centre::addPersonnel(Personnel &personnel)
+{
+
+}
+
+void Centre::deletePatient(Patient &patient)
+{
+
+}
+
+void Centre::deletePersonnel(Personnel &personnel)
+{
+
+}
--- model/typemedecin.cpp
+++ model/typemedecin.cpp
@@ -7,11 +7,6 @@ string &TypeMedecin::toString(TypeMedecin::personnel)
 
 }
 
-int &TypeMedecin::fromString(string &typeMedecin)
-{
-
-}
-
 TypeMedecin::personnel &TypeMedecin::parseFromString(string &typeMedecin)
 {
 
